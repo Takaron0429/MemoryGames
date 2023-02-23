4742bc462fa65b78a313fd485077ef733b9e1c05
Vodenicsár Rajmund Rómeó <vodenicsar.rajmund_romeo@liveedu.mathiasz.hu>
2023-02-23 12:25:54 +0100

    Bug fix

--- MemoryGames/End.Designer.cs
+++ MemoryGames/End.Designer.cs
@@ -36,6 +36,7 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.name = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -129,12 +130,25 @@
             this.panel1.Size = new System.Drawing.Size(580, 95);
             this.panel1.TabIndex = 6;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(347, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 46);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // End
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 403);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -153,6 +167,7 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -166,5 +181,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label name;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- MemoryGames/End.cs
+++ MemoryGames/End.cs
@@ -2,70 +2,75 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Media;
 using System.Windows.Forms;
 
-namespace MemoryGames
-{
-    public partial class End : Form
-    {
-        public End()
-        {
+namespace MemoryGames {
+    public partial class End : Form {
+        public End() {
             InitializeComponent();
             Read();
             OK();
         }
-
+        SoundPlayer soundplayer = new SoundPlayer();
         static List<string> lines = new List<string>();
         static List<string> lines2 = new List<string>();
-        static void Read()
-        {
+        static void Read() {
             StreamReader name = new StreamReader("../../Resources/username.txt");
             StreamReader money = new StreamReader("../../Resources/money.txt");
-            while (!name.EndOfStream && !money.EndOfStream)
-            {
-                try
-                {
+            while (!name.EndOfStream && !money.EndOfStream) {
+                try {
                     lines.Add(name.ReadLine().ToString());
                     lines2.Add(money.ReadLine().ToString());
                 }
-                catch (Exception error)
-                {
+                catch (Exception error) {
                     MessageBox.Show(error.Message);
                 }
             }
             money.Close();
             name.Close();
         }
-        private void OK()
-        {
+        private void OK() {
 
-            foreach (var str in lines)
-            {
+            foreach (var str in lines) {
                 name.Text = str;
 
             }
-            foreach (var str in lines2)
-            {
+            foreach (var str in lines2) {
                 Money.Text = str;
 
             }
-            if (Money.Text == "0 Ft")
-            {
+            if (Money.Text == "0 Ft") {
                 Money.ForeColor = Color.Red;
+                Money.Location = new Point(243, 6);
+                label1.Text = "Vesztettél";
+                label1.ForeColor = Color.Red;
+                soundplayer = new SoundPlayer(@"../../Resources/Kvizjatek_The-Price-is-Right-Losing-Horn-Gaming-Sound-Effect-_HD_.wav");
+                soundplayer.Play();
 
             }
+            else if (Money.Text == "100.000 Ft" || Money.Text == "1.500.000" || Money.Text == "40.000.000") {
+                Money.ForeColor = Color.Green;
+                label1.Text = "Győztél";
+                label1.ForeColor = Color.Orange;
+                soundplayer = new SoundPlayer(@"../../Resources/Resources/Kvizjatek_Audience-Clapping-Sound-Effect.wav");
+                soundplayer.Play();
+            }
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private void button1_Click(object sender, EventArgs e) {
             Menu f2 = new Menu();
             this.Hide();
             f2.ShowDialog();
             this.Close();
         }
 
-        private void name_Click(object sender, EventArgs e)
-        {
+        private void name_Click(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
 
         }
     }
